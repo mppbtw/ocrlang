0951ba5582f8a92bb0d92da2a0d47c11e4459a2f
mppdevv <mppdevv@skiff.com>
2024-04-11 21:16:04 +0100

    Updated the parser tests not to use placeholder expressions

--- lib/parser/test.rs
+++ lib/parser/test.rs
@@ -1,98 +1,104 @@
 use super::parse_from_string;
-use crate::syntax::ExpressionType;
 use crate::syntax::StatementType;
 
 #[test]
 fn test_parse_var_assign_statement() {
     let input = "a = 1
-        global bb = 22
-        ccc = 333";
+global bb = 22
+ccc = 333";
     let prog = parse_from_string(input).unwrap();
-    assert_eq!(prog.statements.len(), 3);
-
+    assert_eq!(prog.statements.len(), input.lines().count());
     assert_eq!(
-        prog.statements[0].pretty_print(),
-        "a = <PLACEHOLDER_EXPRESSION>"
-    );
-
-    assert_eq!(
-        prog.statements[1].pretty_print(),
-        "global bb = <PLACEHOLDER_EXPRESSION>"
-    );
-
-    assert_eq!(
-        prog.statements[2].pretty_print(),
-        "ccc = <PLACEHOLDER_EXPRESSION>"
+        prog.statements
+            .iter()
+            .map(|stmt| stmt.pretty_print())
+            .collect::<Vec<String>>()
+            .join("\n"),
+        input
     );
 }
 
 #[test]
 fn test_parse_identifier_expr() {
-    let input = "foo bar
-        baz";
+    let input = "foo
+bar
+baz";
     let prog = parse_from_string(input).unwrap();
-    assert_eq!(prog.statements.len(), 3);
-
-    assert_eq!(prog.statements[0].pretty_print(), "foo");
-
-    assert_eq!(prog.statements[1].pretty_print(), "bar");
-
-    assert_eq!(prog.statements[2].pretty_print(), "baz");
+    assert_eq!(prog.statements.len(), input.lines().count());
+    assert_eq!(
+        prog.statements
+            .iter()
+            .map(|stmt| stmt.pretty_print())
+            .collect::<Vec<String>>()
+            .join("\n"),
+        input
+    );
 }
 
 #[test]
 fn test_parse_return_statement() {
     let input = "return abc
-        return";
+return";
     let prog = parse_from_string(input).unwrap();
-    assert_eq!(prog.statements.len(), 2);
-
+    assert_eq!(prog.statements.len(), input.lines().count());
     assert_eq!(
-        prog.statements[0].pretty_print(),
-        "return <PLACEHOLDER_EXPRESSION>"
+        prog.statements
+            .iter()
+            .map(|stmt| stmt.pretty_print())
+            .collect::<Vec<String>>()
+            .join("\n"),
+        input
     );
-
-    assert_eq!(prog.statements[1].pretty_print(), "return");
 }
 
 #[test]
 fn test_parse_number_literal_expr() {
-    let input = "123 456";
+    let input = "123
+456";
     let prog = parse_from_string(input).unwrap();
-    assert_eq!(prog.statements.len(), 2);
-    assert_eq!(prog.statements[0].pretty_print(), "123");
-    assert_eq!(prog.statements[1].pretty_print(), "456");
+    assert_eq!(prog.statements.len(), input.lines().count());
+    assert_eq!(
+        prog.statements
+            .iter()
+            .map(|stmt| stmt.pretty_print())
+            .collect::<Vec<String>>()
+            .join("\n"),
+        input
+    );
 }
 
 #[test]
 fn test_parse_bool_expr() {
-    let input = "true true
-
-            false";
+    let input = "true
+true
+false";
     let prog = parse_from_string(input).unwrap();
-    assert_eq!(prog.statements.len(), 3);
-    assert!(matches!(
-        prog.statements[0].get_type(),
-        StatementType::Expression(_)
-    ));
-    if let StatementType::Expression(e) = prog.statements[0].get_type() {
-        assert!(matches!(e.value.get_type(), ExpressionType::Boolean(_)))
-    }
-    assert_eq!(prog.statements[0].pretty_print(), "true");
-    assert_eq!(prog.statements[1].pretty_print(), "true");
-    assert_eq!(prog.statements[2].pretty_print(), "false");
+    assert_eq!(prog.statements.len(), input.lines().count());
+    assert_eq!(
+        prog.statements
+            .iter()
+            .map(|stmt| stmt.pretty_print())
+            .collect::<Vec<String>>()
+            .join("\n"),
+        input
+    );
 }
 
 #[test]
 fn test_parse_prefix_expressions() {
     let input = "NOT true
-        -5
-        +2";
+-5
++2";
     let prog = parse_from_string(input).unwrap();
     assert_eq!(prog.statements.len(), input.lines().count());
-    assert_eq!(prog.statements[0].pretty_print(), "NOT true");
-    assert_eq!(prog.statements[1].pretty_print(), "-5");
-    assert_eq!(prog.statements[2].pretty_print(), "+2");
+    assert_eq!(
+        prog.statements
+            .iter()
+            .map(|stmt| stmt.pretty_print())
+            .collect::<Vec<String>>()
+            .join("\n"),
+        input
+    );
 }
 
 #[test]
